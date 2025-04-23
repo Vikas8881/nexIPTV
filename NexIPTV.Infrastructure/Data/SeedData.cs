b692f60e0190f2cc925f79d931fdbe5b4a7f8468
Vikas <vikasmalik8881@gmail.com>
2025-04-24 03:26:29 +0530

    Nuget Add

--- NexIPTV.Infrastructure/Data/SeedData.cs
+++ NexIPTV.Infrastructure/Data/SeedData.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
